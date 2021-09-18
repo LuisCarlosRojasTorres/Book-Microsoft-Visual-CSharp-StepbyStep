8d31e225f5e2e862e8331f97fde9e20955cc6b7a
Luis Carlos Absalón Rojas Torres <redtower.soft@gmail.com>
2021-09-18 16:20:33 -0300

    Chapter 1
    - Console example using Net Framework

---
+++ CH01/CH01A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System; // used Assemblie
+using System.Collections.Generic; // unused Assemblie
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH01A
+{
+    class Program
+    {
+        //One line comment
+        /*
+         * Multiline
+         * Comment
+         */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Rufo");
+        }
+    }
+}
